2c3712fcba1e5a76ff7fc3be354788c340a1b06d
Roger <rxu@hoopp.com>
2025-05-24 20:00:52 -0400

    Improve ui detail

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -37,11 +37,11 @@ const Header = () => {
         </ul>
         <div className="md:hidden flex items-center">
           <button
-            className="flex md:hidden items-center justify-center w-10 h-10 rounded-full bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 transition duration-300"
+            className="flex md:hidden items-center justify-center w-10 h-10 rounded-full transition duration-300"
             onClick={() => setIsOpen(!isOpen)}
             onMouseEnter={() => playClick()}
           >
-            {isOpen? <CloseIcon className="text-gray-800 dark:text-gray-200"/> : <MenuIcon className="text-gray-800 dark:text-gray-200" />}
+            {isOpen? <CloseIcon /> : <MenuIcon />}
           </button>
         </div>
         {!isOpen && (
